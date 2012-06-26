0e49cd9a8f59fd498ed30a5077d96b201962529a
Andrew McCloud <andrew@amccloud.com>
2012-06-26 11:20:08 -0700

    Using readyState instead of state(). Zepto XHR objects don't have the state() method, so this will work on both jQuery and Zepto.

--- backbone-safesync.js
+++ backbone-safesync.js
@@ -1,9 +1,10 @@
 (function(_, Backbone) {
     var sync = Backbone.sync;
+
     Backbone.sync = function(method, model, options) {
         var lastXHR = model._lastXHR && model._lastXHR[method];
 
-        if ((lastXHR && lastXHR.state() == 'pending') && (options && options.safe !== false))
+        if ((lastXHR && lastXHR.readyState != 4) && (options && options.safe !== false))
             lastXHR.abort('stale');
 
         if (!model._lastXHR)
