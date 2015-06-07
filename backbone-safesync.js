07b3908b30aefb6c906f37c82c45e4d4c5d56375
Taras Remez <taras.remez@telix.com.ua>
2015-06-07 19:45:35 +0300

    fix for url getter

--- backbone-safesync.js
+++ backbone-safesync.js
@@ -4,10 +4,11 @@
 	Backbone.sync = function(method, model, options) {
 		var lastXHR = model._lastXHR && model._lastXHR[method];
 		var lastUrl = model._lastXHR && model._lastXHR['url'];
+		var modelUrl=_.result(model, 'url');
 
 		if ((lastXHR && lastXHR.readyState != 4) && (options && options.safe !== false)){
 			//If this is a same request - we dont want to repeat it and want to wait for finish, if url differs - cancel current
-			if(lastUrl===model.url()){
+			if(lastUrl===modelUrl){
 				return lastXHR;
 			}else{
 				lastXHR.abort('stale');
@@ -19,7 +20,7 @@
 			model._lastXHR = {};
 		}
 
-		model._lastXHR['url']=model.url();
+		model._lastXHR['url']=modelUrl;
 		return model._lastXHR[method] = sync.apply(this, arguments);
 	};
 })(window._, window.Backbone);
