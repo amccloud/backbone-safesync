9153af4d8357a0ea52727fa2070b21afd702c84a
Taras Remez <taras.remez@telix.com.ua>
2015-06-07 19:14:51 +0300

    Let current request to complete, if it requests same url

--- backbone-safesync.js
+++ backbone-safesync.js
@@ -1,15 +1,25 @@
 (function(_, Backbone) {
-    var sync = Backbone.sync;
+	var sync = Backbone.sync;
 
-    Backbone.sync = function(method, model, options) {
-        var lastXHR = model._lastXHR && model._lastXHR[method];
+	Backbone.sync = function(method, model, options) {
+		var lastXHR = model._lastXHR && model._lastXHR[method];
+		var lastUrl = model._lastXHR && model._lastXHR['url'];
 
-        if ((lastXHR && lastXHR.readyState != 4) && (options && options.safe !== false))
-            lastXHR.abort('stale');
+		if ((lastXHR && lastXHR.readyState != 4) && (options && options.safe !== false)){
+			//If this is a same request - we dont want to repeat it and want to wait for finish, if url differs - cancel current
+			if(lastUrl===model.url()){
+				return lastXHR;
+			}else{
+				lastXHR.abort('stale');
+			}
 
-        if (!model._lastXHR)
-            model._lastXHR = {};
+		}
 
-        return model._lastXHR[method] = sync.apply(this, arguments);
-    };
+		if (!model._lastXHR){
+			model._lastXHR = {};
+		}
+
+		model._lastXHR['url']=model.url();
+		return model._lastXHR[method] = sync.apply(this, arguments);
+	};
 })(window._, window.Backbone);
